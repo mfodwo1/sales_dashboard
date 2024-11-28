2a93cfc6532f2da8d9abc10f7a285d4436439be1
Mfodwo Christian <angelic7440@gmail.com>
2024-11-28 06:19:14 +0000

    initial commit

---
+++ app/api/auth/[...nextauth].ts
@@ -0,0 +1,50 @@
+import NextAuth from 'next-auth';
+import CredentialsProvider from 'next-auth/providers/credentials';
+import axios from 'axios';
+
+export default NextAuth({
+  providers: [
+    CredentialsProvider({
+      name: 'Credentials',
+      credentials: {
+        email: { label: 'Email', type: 'email' },
+        password: { label: 'Password', type: 'password' },
+      },
+      async authorize(credentials) {
+        try {
+          const response = await axios.post(
+            'https://rb-playground.onrender.com/internal/auth_login_create',
+            {
+              email: credentials?.email,
+              password: credentials?.password,
+            }
+          );
+
+          if (response.data) {
+            return { ...response.data };
+          }
+
+          return null;
+        } catch (error) {
+          console.error(error);
+          return null;
+        }
+      },
+    }),
+  ],
+  callbacks: {
+    async jwt({ token, user }) {
+      if (user) {
+        token.accessToken = user.accessToken;
+      }
+      return token;
+    },
+    async session({ session, token }) {
+      session.accessToken = token.accessToken;
+      return session;
+    },
+  },
+  pages: {
+    signIn: '/login',
+  },
+});
